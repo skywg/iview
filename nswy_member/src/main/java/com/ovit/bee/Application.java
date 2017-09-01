35d1e248e170e2278c805cc41a0105634f0146d8
wg <786905664@qq.com>
2017-09-01 16:43:25 +0800

    fdfd

--- nswy_member/src/main/java/com/ovit/bee/Application.java
+++ nswy_member/src/main/java/com/ovit/bee/Application.java
@@ -54,6 +54,7 @@ public class Application extends  SpringBootServletInitializer {
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
+		
         return builder.sources(Application.class);
     }
 	
